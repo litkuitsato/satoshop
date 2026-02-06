ea21fc1f0351365afa25043dfbfee17aac049e55
JoseLuis <jose.sg_95@hotmail.com>
2026-02-06 15:59:24 -0500

    MODULO 4

--- web/models.py
+++ web/models.py
@@ -19,3 +19,38 @@ class Producto(models.Model):
     def __str__(self):
         return self.nombre
 
+from django.contrib.auth.models import User
+
+class Cliente(models.Model):
+    user = models.OneToOneField(User, on_delete=models.RESTRICT)
+    dni = models.CharField(max_length=8, null=True, blank= True)
+    sexo = models.CharField(max_length=1 , default='M')
+    direccion = models.CharField(max_length=300, null=True, blank=True)
+    telefono = models.CharField(max_length=20, null=True, blank=True)
+    fecha_nacimiento = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.dni
+
+class Pedido(models.Model):
+    ESTADO_CHOICES = [
+        ('0', 'Solicitado'),
+        ('1', 'Pagado'),
+    ]
+    cliente = models.OneToOneField(Cliente, on_delete=models.RESTRICT)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    nro_pedido = models.CharField(max_length=20, null=True, blank=True)
+    monto_total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    estado = models.CharField(max_length=1,default='0', choices=ESTADO_CHOICES)
+    
+    def __str__(self):
+        return self.nro_pedido
+    
+class PedidoDetalle(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.RESTRICT)
+    producto = models.ForeignKey(Producto, on_delete=models.RESTRICT)
+    cantidad = models.IntegerField(default=1)
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    
+    def __str__(self):
+        return self.producto.nombre
--- web/views.py
+++ web/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Categoria, Producto
+from .models import Categoria, Producto, Cliente
 
 # Create your views here.
 """ VISTA PARA EL CATALOGO DE PRODUCTOS"""
@@ -86,3 +86,146 @@ def limpiarCarrito(request):
     carritoProducto.clear()
 
     return render(request, 'carrito.html')
+
+"""VISTAS PARA CLIENTES Y USUARIOS"""
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .forms import ClienteForm
+from django.contrib.auth.decorators import login_required
+
+def crearUsuario(request):
+    """Vista para crear un nuevo usuario"""
+    if request.method == 'POST':
+        dataUsuario = request.POST['nuevoUsuario']
+        dataPassword = request.POST['nuevoPassword']
+
+        nuevoUsuario = User.objects.create_user(username=dataUsuario, password=dataPassword)
+        if nuevoUsuario is not None:
+            login(request, nuevoUsuario)
+            return redirect('/cuenta/')
+    return render(request, 'login.html')
+
+
+def logoutUsuario(request):
+    """Vista para cerrar sesión del usuario"""
+    logout(request)
+    return render(request,'login.html')
+
+
+def cuentaUsuario(request):
+    """Vista para mostrar la cuenta del usuario"""
+    try:
+        clienteEditar = Cliente.objects.get(user=request.user)
+
+        dataCliente = {
+            'nombre': request.user.first_name,
+            'apellidos': request.user.last_name,
+            'email': request.user.email,
+            'direccion': clienteEditar.direccion,
+            'telefono': clienteEditar.telefono,
+            'dni': clienteEditar.dni,
+            'sexo': clienteEditar.sexo,
+            'fecha_nacimiento': clienteEditar.fecha_nacimiento,
+        }
+    except:
+        dataCliente = {
+            'nombre': request.user.first_name,
+            'apellidos': request.user.last_name,
+            'email': request.user.email,
+        }
+    
+    frmCliente = ClienteForm(dataCliente)
+    context = {
+        'frmCliente': frmCliente,
+    }
+    return render(request, 'cuenta.html', context)
+
+
+def loginUsuario(request):
+    paginaDestino = request.GET.get('next',None)
+    context = {
+        'destino': paginaDestino,
+    }
+
+    if request.method == 'POST':
+        dataUsuario = request.POST['usuario']
+        dataPassword = request.POST['password']
+        dataDestino = request.POST['destino']
+
+        usuarioAuth=authenticate(request, username=dataUsuario, password=dataPassword)
+        if usuarioAuth is not None:
+            login(request, usuarioAuth)
+            if dataDestino != 'None':
+                
+                return redirect(dataDestino)
+            return redirect('/cuenta')
+        else:
+            context= {
+                'mensajeError': 'Usuario o contraseña incorrectos'
+                }
+    return render(request, 'login.html', context)
+
+
+
+def actualizarCliente(request):
+    """Vista para actualizar los datos del cliente"""
+    mensaje = " "
+    if request.method == 'POST':
+        frmCliente = ClienteForm(request.POST)
+        if frmCliente.is_valid():
+            dataCliente = frmCliente.cleaned_data
+
+
+            #actualizamos el usuario
+            actUsuario = User.objects.get(pk=request.user.id)
+            actUsuario.first_name = dataCliente['nombre']
+            actUsuario.last_name = dataCliente['apellidos']
+            actUsuario.email = dataCliente['email']
+            actUsuario.save()
+
+            #registrar Cliente
+            nuevoCliente = Cliente()
+            nuevoCliente.user = actUsuario
+            nuevoCliente.dni = dataCliente['dni']
+            nuevoCliente.direccion = dataCliente['direccion']
+            nuevoCliente.telefono = dataCliente['telefono']
+            nuevoCliente.sexo = dataCliente['sexo']
+            nuevoCliente.fecha_nacimiento = dataCliente['fecha_nacimiento']
+            nuevoCliente.save()
+
+            mensaje = "Datos actualizados correctamente"
+    context = {
+        'mensaje': mensaje,
+        'frmCliente': frmCliente,
+    }
+    return render(request, 'cuenta.html', context)
+
+""" VISTA PARA PROCESO DE COMPRA"""
+@login_required(login_url='/login/')
+def registrarPedido(request):
+    """Vista para registrar un nuevo pedido"""
+    try:
+        clienteEditar = Cliente.objects.get(user=request.user)
+
+        dataCliente = {
+            'nombre': request.user.first_name,
+            'apellidos': request.user.last_name,
+            'email': request.user.email,
+            'direccion': clienteEditar.direccion,
+            'telefono': clienteEditar.telefono,
+            'dni': clienteEditar.dni,
+            'sexo': clienteEditar.sexo,
+            'fecha_nacimiento': clienteEditar.fecha_nacimiento,
+        }
+    except:
+        dataCliente = {
+            'nombre': request.user.first_name,
+            'apellidos': request.user.last_name,
+            'email': request.user.email,
+        }
+        
+    frmCLiente = ClienteForm()
+    context = {
+        'frmCliente': frmCLiente,
+    }
+    return render(request, 'pedido.html', context)
